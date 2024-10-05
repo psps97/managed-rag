9eb746ba2ec2e1391c45b7b18e2e2900acd1ecc1
Kyoungsu Park <kyopark2014@gmail.com>
2024-10-05 21:44:21 +0900

    update

--- cdk-managed-rag/lib/cdk-managed-rag-stack.ts
+++ cdk-managed-rag/lib/cdk-managed-rag-stack.ts
@@ -290,7 +290,7 @@ export class CdkManagedRagStack extends cdk.Stack {
     OpenSearchCollection.addDependency(netPolicy);
 
     const dataAccessPolicy = new opensearchserverless.CfnAccessPolicy(this, `opensearch-data-collection-policy-for-${projectName}`, {
-      name: `data-collection-policy`,
+      name: `data-collection-policy-${projectName}`,
       type: "data",
       policy: JSON.stringify([
         {
