085ea031a6e87333e44330cf1af3e99dfb5c2190
Kyoungsu Park <kyopark2014@gmail.com>
2024-10-06 06:18:00 +0900

    update

--- cdk-managed-rag/lib/cdk-managed-rag-stack.ts
+++ cdk-managed-rag/lib/cdk-managed-rag-stack.ts
@@ -321,7 +321,7 @@ export class CdkManagedRagStack extends cdk.Stack {
           Principal: [
             `arn:aws:iam::${accountId}:role/${knowledge_base_role.roleName}`,
             `arn:aws:iam::${accountId}:role/role-lambda-chat-ws-for-${projectName}-${region}`,
-            //`arn:aws:iam::${accountId}:role/administration`,
+            `arn:aws:iam::${accountId}:role/administration`,
             //`arn:aws:sts::${accountId}:assumed-role/administration/ksdyb-Isengard`,
           ], 
         },
