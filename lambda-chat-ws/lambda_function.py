0fe3ad556bdddbfa831cc2fece75aceece67da9f
Kyoungsu Park <kyopark2014@gmail.com>
2024-10-10 22:17:06 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -132,10 +132,12 @@ def load_secrets():
             SecretId=f"langsmithapikey-{projectName}"
         )
         # print('get_langsmith_api_secret: ', get_langsmith_api_secret)
-        secret = json.loads(get_langsmith_api_secret['SecretString'])
-        #print('secret: ', secret)
-        langsmith_api_key = secret['langsmith_api_key']
-        langchain_project = secret['langchain_project']
+        
+        if 'SecretString' in get_langsmith_api_secret
+            secret = json.loads(get_langsmith_api_secret['SecretString'])
+            #print('secret: ', secret)
+            langsmith_api_key = secret['langsmith_api_key']
+            langchain_project = secret['langchain_project']
     except Exception as e:
         raise e
 
@@ -150,10 +152,12 @@ def load_secrets():
             SecretId=f"tavilyapikey-{projectName}"
         )
         #print('get_tavily_api_secret: ', get_tavily_api_secret)
-        secret = json.loads(get_tavily_api_secret['SecretString'])
-        # print('secret: ', secret)
-        tavily_api_key = json.loads(secret['tavily_api_key'])
-        # print('tavily_api_key: ', tavily_api_key)
+        
+        if 'SecretString' in get_tavily_api_secret:
+            secret = json.loads(get_tavily_api_secret['SecretString'])
+            # print('secret: ', secret)
+            tavily_api_key = json.loads(secret['tavily_api_key'])
+            # print('tavily_api_key: ', tavily_api_key)
     except Exception as e: 
         raise e
     
@@ -162,9 +166,11 @@ def load_secrets():
             SecretId=f"openweathermap-{projectName}"
         )
         #print('get_weather_api_secret: ', get_weather_api_secret)
-        secret = json.loads(get_weather_api_secret['SecretString'])
-        #print('secret: ', secret)
-        weather_api_key = secret['weather_api_key']
+        
+        if 'SecretString' in get_weather_api_secret:
+            secret = json.loads(get_weather_api_secret['SecretString'])
+            #print('secret: ', secret)
+            weather_api_key = secret['weather_api_key']
 
     except Exception as e:
         raise e
