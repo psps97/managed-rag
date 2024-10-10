e4ba9d725de615c2b9720bd93ae8bedeec3d4bfc
Kyoungsu Park <kyopark2014@gmail.com>
2024-10-11 00:06:33 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -121,8 +121,9 @@ reference_docs = []
 secretsmanager = boto3.client('secretsmanager')
 
 tavily_api_key = ""
+weather_api_key = ""
 def load_secrets():
-    global tavily_api_key
+    global tavily_api_key, weather_api_key
     secretsmanager = boto3.client('secretsmanager')
     
     # api key to use LangSmith
@@ -133,11 +134,10 @@ def load_secrets():
         )
         # print('get_langsmith_api_secret: ', get_langsmith_api_secret)
         
-        if get_langsmith_api_secret['SecretString']:
-            secret = json.loads(get_langsmith_api_secret['SecretString'])
-            #print('secret: ', secret)
-            langsmith_api_key = secret['langsmith_api_key']
-            langchain_project = secret['langchain_project']
+        secret = json.loads(get_langsmith_api_secret['SecretString'])
+        #print('secret: ', secret)
+        langsmith_api_key = secret['langsmith_api_key']
+        langchain_project = secret['langchain_project']
     except Exception as e:
         raise e
 
@@ -151,13 +151,13 @@ def load_secrets():
         get_tavily_api_secret = secretsmanager.get_secret_value(
             SecretId=f"tavilyapikey-{projectName}"
         )
-        print('get_tavily_api_secret: ', get_tavily_api_secret)
+        #print('get_tavily_api_secret: ', get_tavily_api_secret)
         
-        if get_tavily_api_secret['SecretString']:
-            secret = json.loads(get_tavily_api_secret['SecretString'])
-            # print('secret: ', secret)
+        secret = json.loads(get_tavily_api_secret['SecretString'])
+        # print('secret: ', secret)
+        if secret['tavily_api_key']:
             tavily_api_key = json.loads(secret['tavily_api_key'])
-            # print('tavily_api_key: ', tavily_api_key)
+        # print('tavily_api_key: ', tavily_api_key)
     except Exception as e: 
         raise e
     
@@ -167,10 +167,9 @@ def load_secrets():
         )
         #print('get_weather_api_secret: ', get_weather_api_secret)
         
-        if get_weather_api_secret['SecretString']:
-            secret = json.loads(get_weather_api_secret['SecretString'])
-            #print('secret: ', secret)
-            weather_api_key = secret['weather_api_key']
+        secret = json.loads(get_weather_api_secret['SecretString'])
+        #print('secret: ', secret)
+        weather_api_key = secret['weather_api_key']
 
     except Exception as e:
         raise e
